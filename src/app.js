fd75bebc290f2de19ce71b43377ba9e673b05fab
AlexCode-dot <alle7000.andersson@gmail.com>
2025-01-21 14:38:17 +0100

    removed comment

--- src/app.js
+++ src/app.js
@@ -1,83 +1,80 @@
 import express from 'express'
-import ejsMate from 'ejs-mate';
+import ejsMate from 'ejs-mate'
 import renderPage from './renderPage.js'
-import { filmExists } from "./fetchMovies.js";
+import { filmExists } from './fetchMovies.js'
 
 export default function initApp(api) {
+  const app = express()
 
-    const app = express()
+  app.engine('ejs', ejsMate)
+  app.set('view engine', 'ejs')
+  app.set('views', './templates')
 
-    app.engine('ejs', ejsMate);
-    app.set('view engine', 'ejs')
-    app.set('views', './templates')
-
-    app.get('/', async (request, response) => {
-        try {
-            const movies = await api.loadMovies();
-            await renderPage(response, 'filmer', { movies })
-        } catch (err) {
-            response.status(500).send('Error loading filmer')
-        }
-    })
-
-    app.get("/movies/:movieId", async (request, response) => {
-        try {
-        const movieId = request.params.movieId;
-        if (!(await filmExists(movieId))) {
-            response.status(404);
-            return renderPage(response, "404", {
-                message: "Filmen kunde inte hittas",
-            });
-        }
-    
-        const movie = await api.loadMovie(movieId);
-        await renderPage(response, 'film', { movie });
-        
+  app.get('/', async (request, response) => {
+    try {
+      const movies = await api.loadMovies()
+      await renderPage(response, 'filmer', { movies })
     } catch (err) {
-        console.error(`Error loading movie with ID ${request.params.movieId}:`, err);
-        response.status(500).send('Error loading the movie');
+      response.status(500).send('Error loading filmer')
     }
-    });
+  })
 
-    app.get('/barnbio', async (request, response) => {
-        try {
-            renderPage(response, 'barnbio')
-        } catch (err) {
-            response.status(500).send('Error loading barnbio')
-        }
-    })
+  app.get('/movies/:movieId', async (request, response) => {
+    try {
+      const movieId = request.params.movieId
+      if (!(await filmExists(movieId))) {
+        response.status(404)
+        return renderPage(response, '404', {
+          message: 'Filmen kunde inte hittas',
+        })
+      }
 
-    app.get('/evenemang', async (request, response) => {
-        try {
-            renderPage(response, 'evenemang')
-        } catch (err) {
-            response.status(500).send('Error loading evenemang')
-        }
-    })
+      const movie = await api.loadMovie(movieId)
+      await renderPage(response, 'film', { movie })
+    } catch (err) {
+      console.error(`Error loading movie with ID ${request.params.movieId}:`, err)
+      response.status(500).send('Error loading the movie')
+    }
+  })
 
-    app.get('/omoss', async (request, response) => {
-        try {
-            renderPage(response, 'omoss')
-        } catch (err) {
-            response.status(500).send('Error loading om oss')
-        }
-    })
+  app.get('/barnbio', async (request, response) => {
+    try {
+      renderPage(response, 'barnbio')
+    } catch (err) {
+      response.status(500).send('Error loading barnbio')
+    }
+  })
 
-    app.get('/loggain', async (request, response) => {
-        try {
-            renderPage(response, 'loggain')
-        } catch (err) {
-            response.status(500).send('Error loading logga in')
-        }
-    })
+  app.get('/evenemang', async (request, response) => {
+    try {
+      renderPage(response, 'evenemang')
+    } catch (err) {
+      response.status(500).send('Error loading evenemang')
+    }
+  })
 
-    app.use('/static', express.static('./static'))
+  app.get('/omoss', async (request, response) => {
+    try {
+      renderPage(response, 'omoss')
+    } catch (err) {
+      response.status(500).send('Error loading om oss')
+    }
+  })
 
-    app.use((request, response) => {
-        response.status(404);
-        renderPage(response, '404', { message: 'Sidan finns inte' });
-    });
+  app.get('/loggain', async (request, response) => {
+    try {
+      renderPage(response, 'loggain')
+    } catch (err) {
+      response.status(500).send('Error loading logga in')
+    }
+  })
 
-    return app;
+  app.use('/static', express.static('./static'))
+
+  app.use((request, response) => {
+    response.status(404)
+    renderPage(response, '404', { message: 'Sidan finns inte' })
+  })
+
+  return app
 }
-       //HOOK NOT WORKING ON THIS FILE
