0dd51f3b09ab83cbbc9ed87fcbc78f3322235485
AlexCode-dot <alle7000.andersson@gmail.com>
2025-01-21 14:42:07 +0100

    format

--- test/movies.test.js
+++ test/movies.test.js
@@ -1,74 +1,62 @@
-import { expect, test } from '@jest/globals';
-import request from 'supertest';
-import initApp from '../src/app.js';
+import { expect, test } from '@jest/globals'
+import request from 'supertest'
+import initApp from '../src/app.js'
 
 test('Verify that movie pages display the correct title', async () => {
-    const mockLoadMovies = async () => [];
-    const mockLoadMovie = async (id) => {
-        if (id === '1') {
-            return { 
-                id: 1, 
-                title: 'Encanto' };
-        }
-        if (id === '2') {
-            return { 
-                id: 2, 
-                title: 'Forrest Gump' };
-        }
-        return null;
-    };
+  const mockLoadMovies = async () => []
+  const mockLoadMovie = async (id) => {
+    if (id === '1') {
+      return {
+        id: 1,
+        title: 'Encanto',
+      }
+    }
+    if (id === '2') {
+      return {
+        id: 2,
+        title: 'Forrest Gump',
+      }
+    }
+    return null
+  }
 
-    const api = {
-        loadMovies: mockLoadMovies,
-        loadMovie: mockLoadMovie,
-    };
+  const api = {
+    loadMovies: mockLoadMovies,
+    loadMovie: mockLoadMovie,
+  }
 
-    const app = initApp(api);
+  const app = initApp(api)
 
-    const response = await request(app)
-        .get('/movies/1')
-        .expect('Content-Type', /html/)
-        .expect(200);
+  const response = await request(app).get('/movies/1').expect('Content-Type', /html/).expect(200)
 
-    expect(response.text).toMatch('Encanto');
+  expect(response.text).toMatch('Encanto')
 
-    const movieResponse2 = await request(app)
-    .get('/movies/2')
-    .expect('Content-Type', /html/)
-    .expect(200);
-    expect(movieResponse2.text).toMatch('Forrest Gump');
-});
+  const movieResponse2 = await request(app).get('/movies/2').expect('Content-Type', /html/).expect(200)
+  expect(movieResponse2.text).toMatch('Forrest Gump')
+})
 
 test('Verify that a non-existing movie page returns a 404 error and with the correct message', async () => {
-    const api = {
-        loadMovies: async () => [],
-        loadMovie: async () => null
-    };
+  const api = {
+    loadMovies: async () => [],
+    loadMovie: async () => null,
+  }
 
-    const app = initApp(api);
+  const app = initApp(api)
 
-    const notFoundResponse = await request(app)
-        .get('/movies/x')
-        .expect('Content-Type', /html/)
-        .expect(404);
+  const notFoundResponse = await request(app).get('/movies/x').expect('Content-Type', /html/).expect(404)
 
-    expect(notFoundResponse.text).toMatch('Filmen kunde inte hittas');
-});
+  expect(notFoundResponse.text).toMatch('Filmen kunde inte hittas')
+})
 
 test('Verify that any non-existing route returns a 404 error with the correct message', async () => {
-    const mockApi = {
-        loadMovies: async () => [],
-        loadMovie: async () => null,
-    };
+  const mockApi = {
+    loadMovies: async () => [],
+    loadMovie: async () => null,
+  }
 
-    const app = initApp(mockApi);
+  const app = initApp(mockApi)
 
-    const notFoundResponse = await request(app)
-        .get('/invalid-route') 
-        .expect('Content-Type', /html/)
-        .expect(404);
+  const notFoundResponse = await request(app).get('/invalid-route').expect('Content-Type', /html/).expect(404)
 
-    expect(notFoundResponse.text).toMatch('Sidan finns inte');
-});
-
-      
+  expect(notFoundResponse.text).toMatch('Sidan finns inte')
+})
